bf38e1e96a1a5b9466f19c134d984b98a5fbc7fc
vioplum-hp <coolzz27@qq.com>
2025-05-27 00:58:34 +0800

    new calibration

--- calibration/calibrate_cam.py
+++ calibration/calibrate_cam.py
@@ -6,21 +6,21 @@ import glob
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*8, 3), np.float32)
-objp[:, :2] = np.mgrid[0:8, 0:6].T.reshape(-1, 2)
+objp = np.zeros((8*11, 3), np.float32)
+objp[:, :2] = np.mgrid[0:11, 0:8].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
 
-images = glob.glob('images/*.png')
+images = glob.glob('images_0526/processed_images/*.png')
 
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (8, 6), None)
+    ret, corners = cv.findChessboardCorners(gray, (11, 8), None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -30,16 +30,17 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        # cv.drawChessboardCorners(img, (10, 7), corners2, ret)
-        # cv.imshow('img', img)
-        # cv.waitKey(500)
+        cv.drawChessboardCorners(img, (11, 8), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
 
 cv.destroyAllWindows()
 
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
     objpoints, imgpoints, gray.shape[::-1], None, None)
 
-img = cv.imread('images/new_1.png')
+img = cv.imread(
+    './images_0526/processed_images/Explorer_HD2K_SN30856488_19-53-10.png')
 
 h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
--- calibration/find_chessboard.py
+++ calibration/find_chessboard.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import scipy
 import os
+import glob
+
 
 def euler_to_rotation_matrix(angles):
     angles_rad = np.radians(angles)
@@ -22,6 +24,7 @@ def euler_to_rotation_matrix(angles):
 
     R = R_z @ R_y @ R_x
     return R
+
 
 def calibrate_eye_hand(R_gripper2base, t_gripper2base, R_target2cam, t_target2cam, eye_to_hand=True):
 
@@ -44,26 +47,43 @@ def calibrate_eye_hand(R_gripper2base, t_gripper2base, R_target2cam, t_target2ca
 
     return R, t
 
+
 mtx = np.array([
-    [1.85391319e+03, 0.00000000e+00, 1.12117879e+03],
-    [0.00000000e+00, 1.86448545e+03, 7.22642502e+02],
+    [1.91763421e+03, 0.00000000e+00, 1.09079277e+03],
+    [0.00000000e+00, 1.91808727e+03, 6.16433708e+02],
     [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
 ])
 
-dist = np.array([[-0.07672638, 0.0087316, 0.00864253, 0.00472594, -0.24129789]])
+dist = np.array(
+    [[-0.06802034,  0.12629689, - 0.0029845, - 0.0030308, - 0.54281616]])
 
 
 if __name__ == "__main__":
-    image_folder = '.'
-    chessboard_size = (8, 6)
-    square_size = 0.0251  # meters
+    image_folder = './images_0526/processed_images'
+    chessboard_size = (11, 8)
+    square_size = 0.01  # meters
 
+    image_paths = glob.glob(os.path.join(image_folder, '*.*'))
+    image_paths.sort(key=lambda x: int(
+        os.path.splitext(os.path.basename(x))[0]))
     images = []
-    for i in range(1, 12):
-        img_path = f"images/new_{i}.png"
+
+    for img_path in image_paths:
         img = cv2.imread(img_path)
-        img_undistorted = cv2.undistort(img, mtx, dist)
-        images.append(img_undistorted)
+        if img is None:
+            print(f"无法读取图片: {img_path}")
+            continue
+
+        h, w = img.shape[:2]
+        new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(
+            mtx, dist, (w, h), 1, (w, h))
+        undistorted = cv2.undistort(
+            # img, mtx, dist)
+            img, mtx, dist, None, new_camera_mtx)
+        # cv2.imshow("undistorted", undistorted)
+        # cv2.waitKey(0)
+
+        images.append(undistorted)
 
     objp = np.zeros((chessboard_size[0] * chessboard_size[1], 3), np.float32)
     objp[:, :2] = np.mgrid[0:chessboard_size[0],
@@ -100,7 +120,8 @@ if __name__ == "__main__":
                 roll = eulerAngles[2][0]
                 # if cnt == 1: print(R_new)
 
-                f.write(f"eye,{tvec[0][0]},{tvec[1][0]},{tvec[2][0]},{roll},{pitch},{yaw}\n")
+                f.write(
+                    f"eye,{tvec[0][0]},{tvec[1][0]},{tvec[2][0]},{roll},{pitch},{yaw}\n")
                 # f.write(f"eye,{tvec[0][0]:.2f},{tvec[1][0]:.2f},{tvec[2][0]:.2f},{yaw:.2f},{pitch:.2f},{roll:.2f}\n")
 
             else:
@@ -109,10 +130,10 @@ if __name__ == "__main__":
     # print(f"结果已保存至 {output_file}")
 
     poses = []
-    with open('hand.txt', 'r') as file:
+    with open('./images_0526/0526.txt', 'r') as file:
         for line in file:
-            parts = line.strip().split(',')
-            
+            parts = line.strip().split(' ')
+
             if len(parts) == 7:
                 try:
                     x = float(parts[1])
@@ -123,7 +144,7 @@ if __name__ == "__main__":
                     rz = float(parts[6])
 
                     poses.append((x, y, z, rx, ry, rz))
-                    
+
                 except ValueError:
                     print(f"数值转换失败，跳过行: {line.strip()}")
 
@@ -137,10 +158,11 @@ if __name__ == "__main__":
         R_gripper2base.append(R)
         t_gripper2base.append(t)
 
-    R_cam2base, t_cam2base = calibrate_eye_hand(R_gripper2base, t_gripper2base, R_target2cam, t_target2cam)
+    R_cam2base, t_cam2base = calibrate_eye_hand(
+        R_gripper2base, t_gripper2base, R_target2cam, t_target2cam)
 
     print(f"R_cam2base:\n{R_cam2base}\nt_cam2base:\n{t_cam2base}")
 
-    # pos_cam = t_target2cam[2]
-    # pos_base = R_cam2base @ pos_cam + t_cam2base
-    # print(f"pos_base: {pos_base}")
+    pos_cam = t_target2cam[2]
+    pos_base = R_cam2base @ pos_cam + t_cam2base
+    print(f"pos_base: {pos_base}")
